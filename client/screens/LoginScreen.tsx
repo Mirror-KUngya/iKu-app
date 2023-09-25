d0b5360061a1903594f341bb3e17a0a2fcfd4170
cjy921004 <ej921004@naver.com>
2023-09-26 03:05:45 +0900

    Feat : add elements at LoginScreen

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -1,11 +1,65 @@
-import {Text, View} from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-const LoginScreen = () => {
+const LoginScreen = ({}) => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.mainImage}
+        source={require('../assets/images/iku_image.png')}
+      />
+      <TextInput
+        placeholder="아이디"
+        style={styles.textInput}
+        returnKeyType={'next'}
+        autoComplete={'email'}
+        blurOnSubmit={false}
+      />
+      <TextInput
+        placeholder="비밀번호"
+        style={styles.textInput}
+        returnKeyType={'done'}
+        autoComplete={'password'}
+        secureTextEntry={true}
+        blurOnSubmit={false}
+      />
+      <View style={styles.loginContainer}>
+        <TouchableOpacity
+          style={[styles.signButton, {backgroundColor: '#003865'}]}
+          onPress={() => {}}>
+          <Text style={styles.textInButton}>로그인</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.signButton]} onPress={() => {}}>
+          <Text style={styles.textInButton}>아이디찾기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.signButton, {backgroundColor: 'white'}]}
+          onPress={() => {}}>
+          <Text style={styles.textInButton}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  mainImage: {},
+  textInput: {},
+  loginContainer: {},
+  buttonContainer: {},
+  signButton: {},
+  textInButton: {},
+});
 export default LoginScreen;

5e9b82bae3b5a7cbdbdef37215a2f4df21b3cf4b
최정윤 <ej921004@naver.com>
2023-10-04 02:52:05 +0900

    Feat : add MissionStatusScreen UI

--- client/screens/MissionStatusScreen.tsx
+++ client/screens/MissionStatusScreen.tsx
@@ -1,40 +1,60 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {RootStackParamList} from '../types';
 import {Calendar} from 'react-native-calendars';
 import colors from '../lib/styles/colors';
 import '../lib/utils/localeConfig';
+import {useState} from 'react';
+import {MissionItem} from '../components/MissionItem';
 type MissionStatustProps = NativeStackScreenProps<
   RootStackParamList,
   'MissionStatusScreen'
 >;
 
 const MissionStatusScreen: React.FC<MissionStatustProps> = ({navigation}) => {
+  const [pickDate, setPickDate] = useState(new Date());
+
+  let missionList = [
+    {text: '박수치기', fulFilled: true},
+    {text: '어깨 스트레칭 하기', fulFilled: false},
+    {text: '어깨 스트레칭 하기', fulFilled: true},
+    {text: '창문 닫기', fulFilled: false},
+    {text: '창문 닫기', fulFilled: false},
+  ];
+
   return (
-    <View style={styles.container}>
-      <Calendar
-        // markedDates={markedDates}
-        // onDayPress={day => console.log(day)}
-        // onMonthChange={month => console.log('month changed', month)}
-        theme={{
-          arrowColor: colors.navy,
-          arrowHeight: 40,
-          textMonthFontSize: 24,
-          textMonthFontWeight: 'bold',
-          todayTextColor: 'white',
-          todayBackgroundColor: colors.orange,
-          textDayFontSize: 22,
-          textDayFontWeight: 'bold',
-        }}
-      />
-    </View>
+    <ScrollView style={styles.container}>
+      <View>
+        <Calendar
+          onDayPress={day => setPickDate(new Date(day.dateString))}
+          theme={{
+            arrowColor: colors.navy,
+            arrowHeight: 40,
+            textMonthFontSize: 24,
+            textMonthFontWeight: 'bold',
+            todayTextColor: 'white',
+            todayBackgroundColor: colors.orange,
+            selectedDayBackgroundColor: 'orange',
+            textDayFontSize: 22,
+            textDayFontWeight: 'bold',
+          }}
+        />
+      </View>
+      <View style={styles.line}></View>
+      <View style={{marginBottom: 10, marginHorizontal: 10}}>
+        <Text style={styles.dateText}>{`${pickDate.getFullYear()}년 ${
+          pickDate.getMonth() + 1
+        }월 ${pickDate.getDate()}일`}</Text>
+      </View>
+      <View>
+        {missionList.map(({text, fulFilled}, idx) => (
+          <MissionItem
+            key={idx}
+            text={text}
+            isFulfilled={fulFilled}></MissionItem>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -42,6 +62,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     padding: 15,
+  },
+  line: {
+    height: 2,
+    backgroundColor: colors.navy,
+    marginHorizontal: 80,
+    marginVertical: 30,
+  },
+  dateText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.navy,
   },
 });
 export {MissionStatusScreen};

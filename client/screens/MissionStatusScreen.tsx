99a2c0302648d49040d2489c2ec0c7c9c2c91c37
최정윤 <ej921004@naver.com>
2023-10-04 02:11:20 +0900

    Feat : add calandar at missionStatusScreen

--- client/screens/MissionStatusScreen.tsx
+++ client/screens/MissionStatusScreen.tsx
@@ -8,14 +8,34 @@ import {
   View,
 } from 'react-native';
 import {RootStackParamList} from '../types';
+import {Calendar} from 'react-native-calendars';
 import colors from '../lib/styles/colors';
-
+import '../lib/utils/localeConfig';
 type MissionStatustProps = NativeStackScreenProps<
   RootStackParamList,
   'MissionStatusScreen'
 >;
+
 const MissionStatusScreen: React.FC<MissionStatustProps> = ({navigation}) => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Calendar
+        // markedDates={markedDates}
+        // onDayPress={day => console.log(day)}
+        // onMonthChange={month => console.log('month changed', month)}
+        theme={{
+          arrowColor: colors.navy,
+          arrowHeight: 40,
+          textMonthFontSize: 24,
+          textMonthFontWeight: 'bold',
+          todayTextColor: 'white',
+          todayBackgroundColor: colors.orange,
+          textDayFontSize: 22,
+          textDayFontWeight: 'bold',
+        }}
+      />
+    </View>
+  );
 };
 const styles = StyleSheet.create({
   container: {

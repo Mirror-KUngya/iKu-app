b1c1e3d01b4ef8209af3e3ea6cfb3cd14da68e23
최정윤 <ej921004@naver.com>
2023-11-06 16:12:37 +0900

    Feat : detect router 추가

--- server/index.js
+++ server/index.js
@@ -1,29 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
-const connectDB = require("./config/mongoDB");
+const connectDB = require('./config/mongoDB');
 
-var userRouter = require("./routes/user");
-var missionRouter = require("./routes/mission");
-var checkListRouter = require("./routes/checkList");
-var settingRouter = require("./routes/setting");
+const userRouter = require('./routes/user');
+const missionRouter = require('./routes/mission');
+const checkListRouter = require('./routes/checkList');
+const settingRouter = require('./routes/setting');
+const detectRouter = require('./routes/detect'); // 인공지능 인식
 
-connectDB.mongoDB() //DB 연결
+connectDB.mongoDB(); //DB 연결
 
-app.use(express.json({extended: false})) // req의body 정보를 읽을 수 있도록 설정
+app.use(cors());
+app.use(express.json({extended: false})); // req의body 정보를 읽을 수 있도록 설정
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 app.use('/users', userRouter);
 app.use('/missions', missionRouter);
 app.use('/checkList', checkListRouter);
 app.use('/setting', settingRouter);
+app.use('/detect', detectRouter);
 
 app.get('/test', (req, res) => {
-    res.json({"test": "success"});
+  res.json({test: 'success'});
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });

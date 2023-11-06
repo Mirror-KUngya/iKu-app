f95f13bdeb6c1c593ae05da6f71d18a6d22419aa
최정윤 <ej921004@naver.com>
2023-11-06 15:42:08 +0900

    Chore : remove unused files

--- client/types 2.ts
+++
@@ -1,8 +0,0 @@
-export type RootStackParamList = {
-  HomeScreen: undefined;
-  LoginScreen: undefined;
-  CheckListScreen: undefined;
-  SettingScreen: undefined;
-  MissionStatusScreen: undefined;
-  SignUpScreen: undefined;
-};

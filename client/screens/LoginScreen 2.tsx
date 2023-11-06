a8fe1b668ec3b3106bdb27a364e372100e52a0ad
최정윤 <ej921004@naver.com>
2023-11-06 15:39:00 +0900

    Feat : add dependency

---
+++ client/screens/LoginScreen 2.tsx
@@ -0,0 +1,117 @@
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import colors from '../lib/styles/colors';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../types';
+
+type LoginProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
+
+const LoginScreen: React.FC<LoginProps> = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.mainImage}
+        source={require('../assets/images/iku_image.png')}
+      />
+      <Text style={styles.mainText}>iKU</Text>
+      <TextInput
+        placeholder="아이디"
+        style={styles.textInput}
+        returnKeyType={'next'}
+        autoComplete={'email'}
+        blurOnSubmit={false}
+      />
+      <TextInput
+        placeholder="비밀번호"
+        style={styles.textInput}
+        returnKeyType={'done'}
+        autoComplete={'password'}
+        secureTextEntry={true}
+        blurOnSubmit={false}
+      />
+      <View>
+        <TouchableOpacity
+          style={[styles.loginButtonContainer]}
+          onPress={() => {}}>
+          <Text
+            style={styles.loginText}
+            onPress={() => {
+              navigation.navigate('HomeScreen');
+            }}>
+            로그인
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.textInButton}>아이디찾기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('SignUpScreen');
+          }}>
+          <Text style={styles.textInButton}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  mainImage: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+  },
+  mainText: {
+    color: colors.green,
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    width: 300,
+    fontSize: 20,
+    justifyContent: 'center',
+    borderBottomWidth: 2,
+    borderColor: colors.navy,
+    margin: 5,
+    fontWeight: 'bold',
+  },
+  loginButtonContainer: {
+    marginTop: 10,
+  },
+  loginText: {
+    width: 300,
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: colors.navy,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    margin: 10,
+  },
+  signButton: {},
+  textInButton: {
+    fontSize: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    fontWeight: 'bold',
+  },
+});
+export {LoginScreen};
---
+++ client/types 2.ts
@@ -0,0 +1,8 @@
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  LoginScreen: undefined;
+  CheckListScreen: undefined;
+  SettingScreen: undefined;
+  MissionStatusScreen: undefined;
+  SignUpScreen: undefined;
+};

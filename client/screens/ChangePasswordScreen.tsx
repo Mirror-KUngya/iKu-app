25b1d71fcff67c4c13a399e111ac3d7fe7468e80
최정윤 <ej921004@naver.com>
2023-11-11 06:01:05 +0900

    Design : add pages UI

--- client/screens/ChangePasswordScreen.tsx
+++ client/screens/ChangePasswordScreen.tsx
@@ -1,6 +1,7 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
 import {
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -19,44 +20,99 @@ const ChangePasswordScreen: React.FC<ChangePasswordProps> = ({navigation}) => {
   const [inputId, setInputId] = useState('');
   const [inputPhone, setInputPhone] = useState('');
   const [inputNewPassword, setInputNewPassword] = useState('');
+  const [hideFirstScreen, setHideFirstScreen] = useState(false); // 첫 화면 숨기기
+  const [result, setResult] = useState(true); // 비밀번호 성공하면 true, 실패하면 false로
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>회원 정보를 입력해주세요.</Text>
-      <TextInput
-        placeholder="아이디"
-        style={styles.textInput}
-        returnKeyType={'done'}
-        blurOnSubmit={false}
-        value={inputId}
-        onChangeText={text => setInputId(text)}
-      />
-      <TextInput
-        placeholder="전화번호"
-        style={styles.textInput}
-        returnKeyType={'done'}
-        blurOnSubmit={false}
-        value={inputPhone}
-        onChangeText={text => setInputPhone(text)}
-      />
-      <TextInput
-        placeholder="새로운 비밀번호"
-        style={styles.textInput}
-        returnKeyType={'done'}
-        autoComplete={'password'}
-        secureTextEntry={true}
-        blurOnSubmit={false}
-        value={inputNewPassword}
-        onChangeText={text => setInputNewPassword(text)}
-      />
-      <TouchableOpacity
-        style={[styles.buttonContainer]}
-        //onPress={onLoginPress}
-        onPress={() => {
-          navigation.navigate('HomeScreen');
+      <View
+        style={{
+          display: hideFirstScreen ? 'none' : 'flex',
         }}>
-        <Text style={styles.buttonText}>변경</Text>
-      </TouchableOpacity>
+        <Text style={styles.titleText}>회원 정보를 입력해주세요.</Text>
+        <TextInput
+          placeholder="아이디"
+          style={styles.textInput}
+          returnKeyType={'done'}
+          blurOnSubmit={false}
+          value={inputId}
+          onChangeText={text => setInputId(text)}
+        />
+        <TextInput
+          placeholder="전화번호"
+          style={styles.textInput}
+          returnKeyType={'done'}
+          blurOnSubmit={false}
+          value={inputPhone}
+          onChangeText={text => setInputPhone(text)}
+        />
+        <TextInput
+          placeholder="새로운 비밀번호"
+          style={styles.textInput}
+          returnKeyType={'done'}
+          autoComplete={'password'}
+          secureTextEntry={true}
+          blurOnSubmit={false}
+          value={inputNewPassword}
+          onChangeText={text => setInputNewPassword(text)}
+        />
+        <TouchableOpacity
+          style={[styles.buttonContainer]}
+          onPress={() => {
+            setHideFirstScreen(true);
+          }}>
+          <Text style={styles.buttonText}>변경</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={[
+          {alignItems: 'center', display: hideFirstScreen ? 'flex' : 'none'},
+        ]}>
+        {result ? (
+          <>
+            <Image
+              style={styles.image}
+              source={require('../assets/images/check.png')}
+            />
+            <Text style={styles.titleText}>비밀번호가 변경되었습니다.</Text>
+            <TouchableOpacity
+              style={[styles.buttonContainer]}
+              onPress={() => {
+                navigation.navigate('LoginScreen');
+              }}>
+              <Text style={styles.buttonText}>로그인 하러 가기</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <Image
+              style={styles.image}
+              source={require('../assets/images/warning.png')}
+            />
+            <Text style={styles.titleText}>
+              존재하지 않는{'\n'}회원 정보입니다.
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                onPress={() => {
+                  setHideFirstScreen(false);
+                }}>
+                <Text style={[styles.buttonText, {width: 160}]}>
+                  이전 페이지로
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('LoginScreen');
+                }}>
+                <Text style={[styles.buttonText, {width: 160}]}>
+                  로그인 하러 가기
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      </View>
     </View>
   );
 };
@@ -67,6 +123,12 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     alignItems: 'center',
     backgroundColor: 'white',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    justifyContent: 'center',
   },
   textInput: {
     width: 300,
@@ -85,8 +147,9 @@ const styles = StyleSheet.create({
   titleText: {
     color: colors.navy,
     fontSize: 24,
-    fontWeight: '900',
+    fontWeight: 'bold',
     margin: 20,
+    textAlign: 'center',
   },
   buttonText: {
     width: 300,
@@ -94,6 +157,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     padding: 10,
+    marginHorizontal: 5,
     borderRadius: 10,
     backgroundColor: colors.navy,
   },
--- client/screens/FindIdScreen.tsx
+++ client/screens/FindIdScreen.tsx
@@ -1,6 +1,7 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
 import {
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -15,33 +16,104 @@ type FindIdProps = NativeStackScreenProps<RootStackParamList, 'FindIdScreen'>;
 const FindIdScreen: React.FC<FindIdProps> = ({navigation}) => {
   const [inputName, setInputName] = useState('');
   const [inputPhone, setInputPhone] = useState('');
-
+  const [hideFirstScreen, setHideFirstScreen] = useState(false); // 첫 화면 숨기기
+  const [result, setResult] = useState(false); // 비밀번호 성공하면 true, 실패하면 false로
+  const [userId, setUserId] = useState('sobok_owner'); // 회원 아이디
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>회원 정보를 입력해주세요.</Text>
-      <TextInput
-        placeholder="이름"
-        style={styles.textInput}
-        returnKeyType={'done'}
-        blurOnSubmit={false}
-        value={inputName}
-        onChangeText={text => setInputName(text)}
-      />
-      <TextInput
-        placeholder="전화번호"
-        style={styles.textInput}
-        returnKeyType={'done'}
-        blurOnSubmit={false}
-        value={inputPhone}
-        onChangeText={text => setInputPhone(text)}
-      />
-      <TouchableOpacity
-        style={[styles.ㅠuttonContainer]}
-        onPress={() => {
-          navigation.navigate('HomeScreen');
+      <View
+        style={{
+          display: hideFirstScreen ? 'none' : 'flex',
         }}>
-        <Text style={styles.buttonText}>찾기</Text>
-      </TouchableOpacity>
+        <Text style={styles.titleText}>회원 정보를 입력해주세요.</Text>
+        <TextInput
+          placeholder="이름"
+          style={styles.textInput}
+          returnKeyType={'done'}
+          blurOnSubmit={false}
+          value={inputName}
+          onChangeText={text => setInputName(text)}
+        />
+        <TextInput
+          placeholder="전화번호"
+          style={styles.textInput}
+          returnKeyType={'done'}
+          blurOnSubmit={false}
+          value={inputPhone}
+          onChangeText={text => setInputPhone(text)}
+        />
+        <TouchableOpacity
+          style={[styles.buttonContainer]}
+          onPress={() => {
+            setHideFirstScreen(true);
+          }}>
+          <Text style={styles.buttonText}>찾기</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          display: hideFirstScreen ? 'flex' : 'none',
+        }}>
+        {result ? (
+          <>
+            <Image
+              style={styles.image}
+              source={require('../assets/images/check.png')}
+            />
+            <Text style={styles.titleText}>
+              회원님의 아이디는{'\n'}
+              {userId}입니다.
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('LoginScreen');
+                }}>
+                <Text style={[styles.buttonText, {width: 160}]}>
+                  로그인 하러 가기
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('ChangePasswordScreen');
+                }}>
+                <Text style={[styles.buttonText, {width: 160}]}>
+                  비밀번호변경
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        ) : (
+          <>
+            <Image
+              style={styles.image}
+              source={require('../assets/images/warning.png')}
+            />
+            <Text style={styles.titleText}>
+              존재하지 않는{'\n'}회원 정보입니다.
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                onPress={() => {
+                  setHideFirstScreen(false);
+                }}>
+                <Text style={[styles.buttonText, {width: 160}]}>
+                  이전 페이지로
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('LoginScreen');
+                }}>
+                <Text style={[styles.buttonText, {width: 160}]}>
+                  로그인 하러 가기
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      </View>
     </View>
   );
 };
@@ -53,6 +125,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+  },
   textInput: {
     width: 300,
     fontSize: 20,
@@ -62,7 +140,7 @@ const styles = StyleSheet.create({
     margin: 5,
     fontWeight: 'bold',
   },
-  ㅠuttonContainer: {
+  buttonContainer: {
     marginTop: 30,
     textAlign: 'center',
     fontSize: 20,
@@ -70,8 +148,9 @@ const styles = StyleSheet.create({
   titleText: {
     color: colors.navy,
     fontSize: 24,
-    fontWeight: '900',
+    fontWeight: 'bold',
     margin: 20,
+    textAlign: 'center',
   },
   buttonText: {
     width: 300,
@@ -79,6 +158,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     padding: 10,
+    marginHorizontal: 5,
     borderRadius: 10,
     backgroundColor: colors.navy,
   },
